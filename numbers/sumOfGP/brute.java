0a2164eb75e89e4a7406378d8feb528943ae9a7c
Shaik <banushaik394@gmail.com>
2025-03-29 20:51:11 +0530

    Sum Of G.P Series

---
+++ numbers/sumOfGP/brute.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class brute {
+    public static float findSumOfAPSeries(int n,float a,float d){
+        float sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=a;
+            a=a*d;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();sc.nextLine();
+        float a=sc.nextFloat();
+        float d=sc.nextFloat();
+        float ans=findSumOfAPSeries(n,a,d);
+        System.out.println(ans);
+        sc.close();
+    }
+}
